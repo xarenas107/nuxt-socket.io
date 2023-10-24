914dd2249043d0c94753706fedc9b7ba3791417d
Xavier Arenas <xarenas107@gmail.com>
2023-10-23 22:57:20 -0500

    chore: update dependences

--- playground/app.vue
+++ playground/app.vue
@@ -1,17 +1,16 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue-demi'
-import { useSocketIO } from '#imports'
+import { useSocketIOStore } from '#imports'
 
 const response = ref('Request from client')
-const io = useSocketIO()
+const io = useSocketIOStore()
 
-
-const onClick = () => $fetch('api/ping')
+const { execute } = useFetch('api/ping',{ immediate:false })
 onMounted(() => io.on('pong',msg => response.value = msg))
 </script>
 
 <template>
-  <button @click="onClick">
+  <button @click="execute()">
     Fetch
   </button>
   <span :style="{ paddingLeft: '1rem' }"> {{ response }} </span>
--- playground/server/api/[...].ts
+++ playground/server/api/[...].ts
@@ -1,7 +1,8 @@
 import { useServerSocketIO } from '#imports'
 
 export default defineEventHandler(event => {
+  console.log('Server route test')
   const socket = useServerSocketIO(event)
-  socket.server.emit('pong', 'Response from socket.io')
+  socket.server.emit('pong', 'Response from server')
   return 'pong'
 })
