2c9a31dcb214c825364f4f26b5d35c17a9bc1815
Xavier Arenas <xarenas107@gmail.com>
2024-01-27 22:26:02 -0500

    chore: improve tests

--- playground/server/plugins/plugin.ts
+++ playground/server/plugins/plugin.ts
@@ -2,7 +2,6 @@ export default defineNitroPlugin(nitro => {
   console.log('Server is running')
 
   nitro.hooks.hook('request',event => {
-    const url = getRequestURL(event)
     console.log(`Request recieved on server`)
   })
 })
