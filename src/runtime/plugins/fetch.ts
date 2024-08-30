595d9d8820a86dfd9f544d4f78015772d4e16232
Xavier <41271402+xarenas107@users.noreply.github.com>
2024-08-30 13:26:41 -0500

    fix(fetch): Solve missing header in certain cases

--- src/runtime/plugins/fetch.ts
+++ src/runtime/plugins/fetch.ts
@@ -1,24 +1,21 @@
 import type { NitroFetchOptions, NitroFetchRequest } from 'nitropack'
-import { defineNuxtPlugin, useRuntimeConfig, useSocketIO } from '#imports'
+import { defineNuxtPlugin, useSocketIO } from '#imports'
 
 export default defineNuxtPlugin({
   dependsOn:['nuxt-socket.io'],
   async setup() {
-    const runtime = useRuntimeConfig()
-
     const options:NitroFetchOptions<NitroFetchRequest> = {
-      baseURL: runtime.app.baseURL,
       onRequest: ({ options }) => {
         const socket = useSocketIO()
 
-        options.headers = {
-          ...options.headers,
-          'io': socket?.id ?? '',
+        if (socket.id) {
+          const io = socket?.id
+          options.headers = { ...options.headers, io }
         }
       }
     }
 
-    const { $fetch } = await import('ofetch')
-    globalThis.$fetch = $fetch.create(options)
+    const $fetch = globalThis.$fetch.create(options)
+    globalThis.$fetch = $fetch
   }
 })
